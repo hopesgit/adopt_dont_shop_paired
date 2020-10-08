ae6bbd232106110dd6a6b92f3f5b67224766ac1b
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-07 20:02:33 -0600

    Add update shelter test

---
+++ spec/features/shelters/update_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  describe "when I visit the shelter show page" do
+    it "I see a link to update the shelter \"Update Shelter\", When I click the link \"Update Shelter\", Then I am taken to '/shelters/:id/edit' where I  see a form to edit the shelter's data including: name, address, city, state, zip" do
+
+      shelter_1 = Shelter.create(name: "Kali's Shelter",
+                                 address: "123 Main St.",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: "12345")
+
+      visit "/shelters/show"
+
+
+      expect(page).to have_link("Update Shelter", href: "/shelters/#{shelter_1.id}/edit")
+
+      find('[type=submit]').click
+
+      expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+      expect(page).to have_field('shelter[name]')
+      expect(page).to have_field('shelter[address]')
+      expect(page).to have_field('shelter[city]')
+      expect(page).to have_field('shelter[state]')
+      expect(page).to have_field('shelter[zip]')
+    end
+  end
+end
