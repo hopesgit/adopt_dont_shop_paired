11778d4622ae8f984728f582cda25c6f41b7498a
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-07 19:41:11 -0600

    Add test for form creates new shelter

---
+++ spec/features/shelters/new_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  describe "when I visit the shelter index page" do
+    it "I see a link to create a new Shelter, 'New Shelter', that when clicked takes me to '/shelters/new' where I  see a form for a new shelter" do
+
+      visit "/shelters"
+
+      expect(page).to have_link("New Shelter", href: '/shelters/new')
+    end
+  end
+end
+
+describe "As a visitor" do
+  describe "When I fill out the form with a new shelter's: name, address, city, state, zip, And I click the button 'Create Shelter' to submit the form" do
+    it "Then a `POST` request is sent to '/shelters', a new shelter is created, and I am redirected to the Shelter Index page where I see the new Shelter listed." do
+
+      visit "/shelters/new"
+      fill_in('shelter[name]', :with => 'Shelter Name')
+      fill_in('shelter[address]', :with => '123 Main St.')
+      fill_in('shelter[city]', :with => 'San Francisco')
+      fill_in('shelter[state]', :with => 'CA')
+      fill_in('shelter[zip]', :with => '94115')
+      find('[type=submit]').click
+
+      expect(current_path).to eq('/shelters')
+      expect(page).to have_content("94115")
+    end
+  end
+end
