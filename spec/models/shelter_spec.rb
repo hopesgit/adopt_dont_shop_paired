ed2262f02b6426117b71546c298ae994f128a277
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-08 17:24:36 -0600

    Add relationship between pets and shelter

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -4,4 +4,8 @@ describe Shelter, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
   end
+
+  describe "relationships" do
+    it {should have_many :pets}
+  end
 end
