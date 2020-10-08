02ede25de01bdc467944c2b9c493793058ba1ff3
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-08 17:18:51 -0600

    Add test for name validation for shelter

---
+++ db/migrate/20201008230912_create_pets.rb
@@ -0,0 +1,10 @@
+class CreatePets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.integer :age
+      t.string :sex
+      t.string :image
+    end
+  end
+end
