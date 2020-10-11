7e42de49ec7238cca433a52950311efce1a276b8
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-11 11:06:43 -0600

    Add test and link to Pets page from New Shelter page

--- spec/features/shelters/update_spec.rb
+++ spec/features/shelters/update_spec.rb
@@ -56,3 +56,17 @@ describe "As a visitor" do
     end
   end
 end
+
+describe "when I visit the Shelters Update Page" do
+  it "there is a link at the top to the Pets Index Page" do
+    shelter_1 = Shelter.create(name: "Kali's Shelter",
+                            address: "123 Main St.",
+                               city: "Denver",
+                              state: "CO",
+                                zip: "12345")
+
+    visit "/shelters/#{shelter_1.id}/edit"
+
+    expect(page).to have_link("Pets", href: '/pets')
+  end
+end
