ad92b7481ddbb9371acef01c00a79c957fa5f5d8
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-11 11:12:37 -0600

    Add test and link to Pets from New Pet page

--- spec/features/pets/new_spec.rb
+++ spec/features/pets/new_spec.rb
@@ -55,3 +55,17 @@ require 'rails_helper'
       expect(page).to have_css("img[src*='https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg']")
     end
   end
+
+  describe "when I visit the New Pet Page" do
+    it "there is a link at the top to the Pets Index Page" do
+      shelter_1 = Shelter.create(name: "Kali's Shelter",
+                              address: "123 Main St.",
+                                 city: "Denver",
+                                state: "CO",
+                                  zip: "12345")
+
+      visit "/shelters/#{shelter_1.id}/pets/new"
+
+      expect(page).to have_link("Pets", href: '/pets')
+    end
+  end
