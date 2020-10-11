3b7fe40e026b02d34c80f76ecb2cceb4dced11ab
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-11 09:21:06 -0600

    Add test update pet in pets index

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -42,5 +42,38 @@ describe "Pet index page" do
     expect(page).to have_css("img[src*='https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg']")
     expect(page).to have_css("img[src*='https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg']")
   end
+end
 
+describe "Pets Index page" do
+  it "has a link to edit pet's info next to every pet, and when I click the link I should be taken to that pets edit page where I can update its information" do
+    shelter_1 = Shelter.create(name: "Kali's Shelter",
+                            address: "123 Main St.",
+                               city: "Denver",
+                              state: "CO",
+                                zip: "12345")
+    pet_1 = shelter_1.pets.create(name: "Kali",
+                                   age: 2,
+                                   sex: "female",
+                                 image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg")
+    pet_2 = shelter_1.pets.create(name: "Pepper",
+                                   age: 3,
+                                   sex: "male",
+                                 image: "https://images.unsplash.com/photo-1548681528-6a5c45b66b42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80")
+    visit '/pets'
+
+    expect(page).to have_css('.update', count: 2)
+
+    click_link("Update Pet", href: "/pets/#{pet_1.id}/edit")
+    fill_in('name', :with => 'Boots')
+    fill_in('age', :with => '4')
+    fill_in('sex', :with => 'male')
+    fill_in('image', :with => 'https://images.unsplash.com/photo-1548681528-6a5c45b66b42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')
+    find('[type=submit]').click
+
+    expect(current_path).to eq("/pets/#{pet_1.id}")
+    expect(page).to have_content("Boots")
+    expect(page).to have_content("4")
+    expect(page).to have_content("male")
+    expect(page).to have_css("img[src*='https://images.unsplash.com/photo-1548681528-6a5c45b66b42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80']")
+  end
 end
