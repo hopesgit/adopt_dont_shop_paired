be4b82b7ac5de2e86af3f8f2095810dc4062b57b
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-13 16:50:29 -0600

    Add test reviews appear on user show page

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -17,5 +17,24 @@ describe "As a visitor" do
       expect(page).to have_content("CO")
       expect(page).to have_content("80205")
     end
+
+    it "Then I see every review this User has written including the review's title, rating, and content" do
+      user_1 = User.create(name: "Sally Peach",
+                 street_address: "123 Main St.",
+                           city: "Denver",
+                          state: "CO",
+                            zip: "80205")
+      review_1 = user_1.review.create(title: "Sally's Review",
+                              rating: 4,
+                             content: "Nice place to find a great pet!",
+                             picture: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg")
+
+      visit "/users/#{user_1.id}"
+
+      expect(page).to have_content("Sally's Review")
+      expect(page).to have_content(4)
+      expect(page).to have_content("Nice place to find a great pet!")
+      expect(page).to have_css("img[src*='https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg']")
+    end
   end
 end
