913914bf5c0bb834423a8c5db0b278c88c4aebc2
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-12 18:45:14 -0600

    Create user table and add test can show user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_010923) do
+ActiveRecord::Schema.define(version: 2020_10_13_003550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,17 @@ ActiveRecord::Schema.define(version: 2020_10_09_010923) do
     t.string "city"
     t.string "state"
     t.integer "zip"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "street_address"
+    t.string "city"
+    t.string "string"
+    t.string "state"
+    t.string "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "pets", "shelters"
