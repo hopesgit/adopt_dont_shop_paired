b23eaa236208a0f7e252a8f5df9928be5c9080a5
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-13 19:21:01 -0600

    Add view reviews on user page

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -19,15 +19,23 @@ describe "As a visitor" do
     end
 
     it "Then I see every review this User has written including the review's title, rating, and content" do
+      shelter_1 = Shelter.create(name: "Kali's Shelter",
+                              address: "123 Main St.",
+                                 city: "Denver",
+                                state: "CO",
+                                  zip: "12345")
       user_1 = User.create!(name: "Sally Peach",
                  street_address: "123 Main St.",
                            city: "Denver",
                           state: "CO",
                             zip: "80205")
-      review_1 = user_1.review.create!(title: "Sally's Review",
-                              rating: 4,
-                             content: "Nice place to find a great pet!",
-                             picture: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg")
+      review_1 = Review.create!({title: "Sally's Review",
+                                rating: 4,
+                               content: "Nice place to find a great pet!",
+                               picture: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
+                               user_id: "#{user_1.id}",
+                            shelter_id: "#{shelter_1.id}"
+                             })
 
       visit "/users/#{user_1.id}"
 
