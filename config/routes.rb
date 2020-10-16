5a3ebee002c08804ec4b7170396596d2d007e19c
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-15 22:49:38 -0600

    Add route for shelter review deletion

--- config/routes.rb
+++ config/routes.rb
@@ -26,9 +26,9 @@ Rails.application.routes.draw do
   post '/shelters/:shelter_id/reviews', to: 'reviews#create'
   get '/shelters/:shelter_id/reviews/:review_id/edit', to: 'reviews#edit'
   patch '/shelters/:shelter_id/reviews/:review_id', to: 'reviews#update'
+  delete '/shelters/:shelter_id/reviews/:review_id', to: 'reviews#destroy'
 
   get '/users/new', to: 'users#new'
   post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show'
-
 end
