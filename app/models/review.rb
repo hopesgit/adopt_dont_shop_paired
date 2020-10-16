155e2f1b196b7da28b9b57cd44814df7aa21e09a
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-16 16:36:57 -0600

    Add test validations for title, rating and content

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,10 @@ class Review < ApplicationRecord
   belongs_to :shelter
   belongs_to :user
 
+  validates :title, presence: true
+  validates :rating, presence: true
+  validates :content, presence: true
+
   def self.user_id(user_name)
     User.select(:name == user_name).limit(1).first.id
   end
