fe721babe055ccb6ef162a7d47ba3cb11975472d
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-17 13:09:52 -0600

    Remove requirements from test and review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,10 +2,6 @@ class Review < ApplicationRecord
   belongs_to :shelter
   belongs_to :user
 
-  # validates :title, presence: true
-  # validates :rating, presence: true
-  # validates :content, presence: true
-
   def self.user_id(user_name)
     User.select(:name == user_name).limit(1).first.id
   end
