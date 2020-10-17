c981677ba9be890c0f6b64b800af83236d6948f2
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-17 17:34:40 -0600

    Add validation test

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Application, type: :model do
     it { should have_many :application_pets }
     it { should have_many(:pets).through(:application_pets) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :status }
+  end
 end
