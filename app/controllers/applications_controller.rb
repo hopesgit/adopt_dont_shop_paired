2e8e24cead97fd0aaa7dabbab0189ccaccc548a8
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-18 10:36:37 -0600

    Add description field when creating new application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -8,10 +8,10 @@ class ApplicationsController < ApplicationController
   end
 
   def create
-    binding.pry
     user = User.find_by(name: params[:user_name])
     application = Application.create!(status: "In Progress",
-                                      user_id: user.id)
-    redirect_to "applications/#{application.id}"
+                                      user_id: user.id,
+                                      description: "")
+    redirect_to "/applications/#{application.id}"
   end
 end
