f66aa170f5b6bb0d9ff11c9e57b15b0758e5961d
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-17 18:11:00 -0600

    Add user name by id method

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,4 +3,8 @@ class Application < ApplicationRecord
   has_many :pets, through: :application_pets
 
   validates_presence_of :description, :status
+
+  def user_name(id)
+    User.find(id).name
+  end
 end
