54a01d32167ee6f9fe877bd3aebfd5776c02d2d5
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-18 10:19:51 -0600

    Add new and create methods to applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,4 +3,15 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    binding.pry
+    user = User.find_by(name: params[:user_name])
+    application = Application.create!(status: "In Progress",
+                                      user_id: user.id)
+    redirect_to "applications/#{application.id}"
+  end
 end
