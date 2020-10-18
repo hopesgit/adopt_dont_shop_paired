47d22ae2a92df69b78d96714ba7b17d569e5df9e
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-17 18:10:35 -0600

    Add user_name method test

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -10,4 +10,39 @@ RSpec.describe Application, type: :model do
     it { should validate_presence_of :description }
     it { should validate_presence_of :status }
   end
+
+  describe "methods" do
+    it "#user_name" do
+      user = User.create!({
+                         name: "Truck Johnson",
+               street_address: "333 Balloon Way",
+                         city: "Heck",
+                        state: "AR",
+                          zip: 65423
+                            })
+      shelter_1 = Shelter.create(name: "Kali's Shelter",
+                              address: "123 Main St.",
+                                 city: "Denver",
+                                state: "CO",
+                                  zip: "12345")
+      pet_1 = shelter_1.pets.create!(name: "Kali",
+                                     age: 2,
+                                     sex: "female",
+                             description: "Cute and sassy cat",
+                                  status: "Adoptable",
+                                   image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg")
+      pet_2 = shelter_1.pets.create!(name: "Ryan",
+                                     age: 3,
+                                     sex: "male",
+                             description: "love bites are the only bites",
+                                  status: "Adoptable",
+                                   image: "https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80")
+      application = Application.create!(user_id: user.id,
+                          description: "I'll be a great pet owner!",
+                          status: "Pending")
+
+      expect(application.user_name(application.user_id)).to eq("Truck Johnson")
+    end
+
+  end
 end
