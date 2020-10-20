3d979fa714c756c5c39b5df6711e881ac631b59b
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-19 19:12:46 -0600

    Add statement to approve and reject pets as admin

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -3,7 +3,11 @@ class ApplicationPetsController < ApplicationController
   def update
     application = Application.find(params[:id])
     app_pet = ApplicationPet.where("pet_id = #{params[:pet_id]}")
-    app_pet.update(application_pet_status: "Approved")
+    if params[:judge]  == "Approve"
+      app_pet.update(application_pet_status: "Approved")
+    else
+      app_pet.update(application_pet_status: "Rejected")
+    end
     redirect_to "/admin/applications/#{params[:id]}"
   end
 
