c415fd4fa33debad5a0d122e3cb7ad3712b7ca18
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-21 13:09:55 -0600

    Update model for validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,9 @@ class Review < ApplicationRecord
   belongs_to :shelter
   belongs_to :user
 
+  validates_presence_of :title, :rating, :content, :user
+
   def self.user_id(user_name)
     User.select(:name == user_name).limit(1).first.id
   end
-
 end
