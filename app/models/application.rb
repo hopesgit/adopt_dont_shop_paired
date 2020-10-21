5033495b7274120da358efd3b906272721b6a416
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-20 19:23:26 -0600

    Add can adopt pets and change application status

--- app/models/application.rb
+++ app/models/application.rb
@@ -7,4 +7,18 @@ class Application < ApplicationRecord
   def find_user(id)
     User.find(id)
   end
+
+  def approved?
+    application_pets.all? {|pet| pet.application_pet_status == "Approved"}
+  end
+
+  def rejected?
+    application_pets.any? {|pet| pet.application_pet_status == "Rejected"}
+  end
+
+  def adopt_pets
+    pets.map do |pet|
+      pet.adopt
+    end
+  end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,4 +4,8 @@ class Pet < ApplicationRecord
   has_many :applications, through: :application_pets
 
   validates_presence_of :name, uniqueness: {case_sensitive: false}
+
+  def adopt
+    self.update(status: "Adopted")
+  end
 end
