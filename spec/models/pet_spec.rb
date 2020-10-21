2d9d2b3fa8af1cc7a583c5d1b1d854f5922ebbf1
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-21 08:22:45 -0600

    Add test for checking specific pet application status by application id

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -39,4 +39,33 @@ describe Pet, type: :model do
       expect(pet_1.adopt).to eq(true)
     end
   end
+
+  describe "#app_status(id)" do
+    it "gives application pet status" do
+      user = User.create!({
+                         name: "Truck Johnson",
+               street_address: "333 Balloon Way",
+                         city: "Heck",
+                        state: "AR",
+                          zip: 65423
+                            })
+      shelter_1 = Shelter.create(name: "Kali's Shelter",
+                              address: "123 Main St.",
+                                 city: "Denver",
+                                state: "CO",
+                                  zip: "12345")
+      pet_1 = shelter_1.pets.create!(name: "Kali",
+                                     age: 2,
+                                     sex: "female",
+                             description: "Cute and sassy cat",
+                                  status: "Adoptable",
+                                   image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg")
+      application = Application.create!(user_id: user.id,
+                          description: "I'll be a great pet owner!",
+                          status: "In Progress")
+      ApplicationPet.create!(pet_id: pet_1.id, application_id: application.id, application_pet_status: "Approved")
+
+      expect(pet_1.app_status(application.id)).to eq("Approved")
+    end
+  end
 end
