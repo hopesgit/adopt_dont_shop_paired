12ab91884547c839753fcf3de964a87e6b237427
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-21 14:08:58 -0600

    Remove unused method

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,4 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :title, :rating, :content, :user
-
-  def self.user_id(user_name)
-    User.select(:name == user_name).limit(1).first.id
-  end
 end
