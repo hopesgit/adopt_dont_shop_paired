0ce8913d046a079482a2edc250b3b465ad736e56
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-21 08:23:07 -0600

    Add check application pet status method

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -8,4 +8,8 @@ class Pet < ApplicationRecord
   def adopt
     self.update(status: "Adopted")
   end
+
+  def app_status(id)
+    self.application_pets.where("application_id = #{id}").pluck(:application_pet_status).first
+  end
 end
