96599f9fc6bbafa1166519572d055ce736c074d4
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-20 19:21:50 -0600

    Add test can change adopt status

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -10,4 +10,33 @@ describe Pet, type: :model do
     it { should have_many :application_pets }
     it { should have_many(:applications).through(:application_pets) }
   end
+
+  describe "#adopt(pet_id)" do
+    it "changes to Adopted status from Adoptable" do
+      user = User.create!({
+                         name: "Truck Johnson",
+               street_address: "333 Balloon Way",
+                         city: "Heck",
+                        state: "AR",
+                          zip: 65423
+                            })
+      shelter_1 = Shelter.create(name: "Kali's Shelter",
+                              address: "123 Main St.",
+                                 city: "Denver",
+                                state: "CO",
+                                  zip: "12345")
+      pet_1 = shelter_1.pets.create!(name: "Kali",
+                                     age: 2,
+                                     sex: "female",
+                             description: "Cute and sassy cat",
+                                  status: "Adoptable",
+                                   image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg")
+      application = Application.create!(user_id: user.id,
+                          description: "I'll be a great pet owner!",
+                          status: "In Progress")
+      ApplicationPet.create!(pet_id: pet_1.id, application_id: application.id)
+
+      expect(pet_1.adopt).to eq(true)
+    end
+  end
 end
